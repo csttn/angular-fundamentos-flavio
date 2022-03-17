a9ba6bcda2c7d1bfac35bddca65ad540aa714035
Cleyton Santana <csttn.dev@gmail.com>
2022-03-17 11:12:01 -0300

    fix: finalizando estrutura do componente commets

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './photo-details.component.html',
-  styleUrls: ['./photo-details.component.css'],
 })
 export class PhotoDetailsComponent implements OnInit {
   photo$!: Observable<IPhoto>;
